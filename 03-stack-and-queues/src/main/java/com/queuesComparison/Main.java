1d7baff8fa0d129bd04e5538bfe46becaaa76958
huangweiyue <huangweiyue@quyiyuan.com>
2019-06-01 21:06:55 +0800

    循环队列  数组队列 比较

---
+++ 03-stack-and-queues/src/main/java/com/queuesComparison/Main.java
@@ -0,0 +1,40 @@
+package com.queuesComparison;
+
+import java.util.Random;
+
+/***
+ * @description 比较循环队列 数组队列
+ * @author huangweiyue
+ * @date Created in 2019/6/1-21:06
+ */
+public class Main {
+
+    // 测试使用q运行opCount个enqueueu和dequeue操作所需要的时间，单位：秒
+    private static double testQueue(Queue<Integer> q, int opCount){
+
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for(int i = 0 ; i < opCount ; i ++)
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        for(int i = 0 ; i < opCount ; i ++)
+            q.dequeue();
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+
+        int opCount = 100000;
+
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
+    }
+}
